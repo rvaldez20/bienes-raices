36ec08edb4479a2524ae7a6da390053257c2173b
rvaldez20 <rvaldez20@gmail.com>
2024-06-08 20:47:59 -0600

    [ADD] implement dark mode for now, only in index.html

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 
    eventListeners()
+
+   darkMode()
 })
 
 function eventListeners() {
@@ -19,4 +21,13 @@ function navegacionResponsive() {
    // } else {
    //    navegacion.classList.add('mostrar')
    // }  es lo mismo que con toogle
+}
+
+function darkMode() {
+   const botonDarkMode = document.querySelector('.dark-mode-boton');
+   botonDarkMode.addEventListener('click', toggleDarkMode)
+}
+
+function toggleDarkMode() {
+   document.body.classList.toggle('dark-mode')
 }
