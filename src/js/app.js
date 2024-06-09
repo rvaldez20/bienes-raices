200a63c189ece1d6189c202a668d69a5eb128186
rvaldez20 <rvaldez20@gmail.com>
2024-06-09 01:17:15 -0600

    [ADD] add dark mode for change when change in SO this prefer

--- src/js/app.js
+++ src/js/app.js
@@ -24,6 +24,23 @@ function navegacionResponsive() {
 }
 
 function darkMode() {
+   const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+   // console.log(prefiereDarkMode.matches)
+
+   if(prefiereDarkMode.matches) {
+      document.body.classList.add('dark-mode');
+   } else {
+      document.body.classList.remove('dark-mode');
+   }
+
+   prefiereDarkMode.addEventListener('change', function() {
+      if(prefiereDarkMode.matches) {
+         document.body.classList.add('dark-mode');
+      } else {
+         document.body.classList.remove('dark-mode');
+      }
+   })
+
    const botonDarkMode = document.querySelector('.dark-mode-boton');
    botonDarkMode.addEventListener('click', toggleDarkMode)
 }
