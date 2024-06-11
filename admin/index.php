e1a3469b115b3b32091a19678b41131c2e844426
rvaldez20 <rvaldez20@gmail.com>
2024-06-11 15:38:46 -0600

    [ADD] in admin index create table for show properties, add styles

--- admin/index.php
+++ admin/index.php
@@ -16,6 +16,33 @@
       <?php endif ?>
 
       <a href="/admin/propiedades/crear.php" class="boton-amarillo">Nueva Propiedad</a>
+
+      <table class="propiedades">
+         <thead>
+            <tr>
+               <th>ID</th>
+               <th>Titulo</th>
+               <th>Imagen</th>
+               <th>Precio</th>
+               <th>Acciones</th>
+            </tr>
+         </thead>
+
+         <tbody>
+            <tr>
+               <td>1</td>
+               <td>Casa en la palya</td>
+               <td><img src="/imagenes/072b975ee658d53f7f2c7184de9cd11a.jpg" alt="imagen propiedad" class="imagen-tabla"></td>
+               <td>$200,000</td>
+               <td>
+                  <a href="#" class="boton-rojo-block">Eliminar</a>
+                  <a href="#" class="boton-amarillo-block">Actualizar</a>
+               </td>
+            </tr>
+         </tbody>
+
+      </table>
+
    </main>
 
 <?php
